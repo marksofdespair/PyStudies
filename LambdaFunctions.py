098830770101d508aae00e4dfe6b710094c881ab
Dia <142447820+marksofdespair@users.noreply.github.com>
2025-04-14 12:18:00 -0500

    Update LambdaFunctions.py

--- LambdaFunctions.py
+++ LambdaFunctions.py
@@ -9,3 +9,20 @@ def square(x):
 # Lambda function 
 
 square_lambda = lambda x: x ** 2 
+
+# Basic syntax
+lambda [arguments]: [expression] 
+
+# Simple Examples
+# Lambda function to add two numbers 
+
+add = lambda a, b: a + b 
+
+print(add(3, 5))  # Output: 8 
+
+# Lambda function to print a name 
+
+greeting = lambda name: f"Hello, {name}!" 
+
+print(greeting("Alice"))  # Output: Hello, Alice! 
+
