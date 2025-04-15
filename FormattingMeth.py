619efed356a1f0c292a1a93105442604d9470b28
Dia <142447820+marksofdespair@users.noreply.github.com>
2025-04-15 16:58:55 -0500

    Update FormattingMeth.py

--- FormattingMeth.py
+++ FormattingMeth.py
@@ -38,3 +38,16 @@ print(man_its_a_hot_one.split())
 line_one = "The sky has given over"
 
 line_one_words = line_one.split()
+
+# Ex
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(',')
+
+print(author_names)
+
+author_last_names = []
+for name in author_names:
+  author_last_names.append(name.split()[-1])
+  
+print(author_last_names)
